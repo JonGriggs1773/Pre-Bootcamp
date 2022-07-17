d50129d6c864c589ed7c006d23413e849741676f
Jon Griggs <104811648+JonGriggs1773@users.noreply.github.com>
2022-07-17 00:00:13 -0500

    Add files via upload

---
+++ DojoWeather/script.js
@@ -0,0 +1,14 @@
+// console.log('Its works!!!')
+
+
+function displayAlert() {
+    alert("It's always sunny here")
+}
+
+
+var cookie = document.querySelector("#foot")
+
+
+function pleaseRemove() {
+    cookie.remove()
+}
